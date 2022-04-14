bc459f326f08ac96ee31ea2563d707232ebea1d1
Organ94 <pechin1994@mail.ru>
2022-04-14 19:46:18 +0300

    Creating folders and files

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -6,6 +8,7 @@ public class Main {
 
     private static final String DIR = "D://Games";
     private static final StringBuilder SB = new StringBuilder();
+    private static final String EXECEPT = "Что-то пошло не так ':-('";
 
     public static void main(String[] args) {
 
@@ -14,5 +17,38 @@ public class Main {
                 DIR + "/res/drawables", DIR + "/res/vectors", DIR + "/res/icons");
 
         List<String> filePath = Arrays.asList(DIR + "/src/main/Main.Java", DIR + "/src/main/Utils.java");
+
+        for (String dir : dirPath) {
+            File dirs = new File(dir);
+            writeToTheDirAndFile(dirs, dirs.mkdir());
+        }
+
+        for (String file : filePath) {
+            try {
+                File files = new File(file);
+                writeToTheDirAndFile(files, files.createNewFile());
+            } catch (IOException e) {
+                System.out.println(EXECEPT);
+            }
+        }
+
+        writeToTheSB(new File(DIR + "/temp/temp.txt"));
+    }
+
+    private static void writeToTheDirAndFile(File file, boolean result) {
+        SB.append(file.isDirectory() ? "Каталог " : "Файл ")
+                .append(file.getName())
+                .append(result ? " создан" : " не создан")
+                .append((!result && file.exists()) ? ", потому что уже существует!" : "")
+                .append((!result && file.exists()) ? ", потому что не верно указан путь!" : "")
+                .append("\n");
+    }
+
+    private static void writeToTheSB(File file) {
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(SB.toString());
+        } catch (IOException e) {
+            System.out.println(EXECEPT);
+        }
     }
 }
