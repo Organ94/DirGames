3768234de1ad155faf9d135e0b4354e0cad387c4
Organ94 <pechin1994@mail.ru>
2022-04-14 19:32:51 +0300

    Added a list of folders and files

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
+    private static final String DIR = "D://Games";
+    private static final StringBuilder SB = new StringBuilder();
+
+    public static void main(String[] args) {
+
+        List<String> dirPath = Arrays.asList(DIR + "/src", DIR + "/res", DIR + "/savegames", DIR + "/temp",
+                DIR + "/src/main", DIR + "/src/Test",
+                DIR + "/res/drawables", DIR + "/res/vectors", DIR + "/res/icons");
+
+        List<String> filePath = Arrays.asList(DIR + "/src/main/Main.Java", DIR + "/src/main/Utils.java");
+    }
 }
